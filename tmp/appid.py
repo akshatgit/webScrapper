0aa514bc3379448f62864fc752df7b08d6bc86b8
akshatgit <akshat14132@iiitd.ac.in>
2021-03-19 21:13:32 -0500

    Stats for exact appid across play stores#2

--- tmp/appid.py
+++ tmp/appid.py
@@ -11,17 +11,25 @@ def main():
 
     cur.execute(query)
     result = cur.fetchall()
-    print(len(result))
+    # print(len(result))
     # print(type(result))
-    print(result[0])
+    # print(result[0])
     stats = dict()
     for app in result:
         appid = app[0].split("/")[-1]
-        if appid in stats:
-            stats[appid]+=1
-        else:
-            stats[appid] = 1
+        if len(appid) > 0:
+            if appid in stats:
+                stats[appid]+=1
+            else:
+                stats[appid] = 1
     sorted_stats = dict(sorted(stats.items(), key=lambda x: x[1], reverse=True))
+    count = 0
+    for i in stats:
+        if stats[i] > 1:
+            count+=1
+
+    print("Same Apps with appid across different app stores: %d" % count)
+
     with open('./tmp/stats.yml', 'w') as outfile:
         yaml.dump(sorted_stats, outfile, sort_keys=False)    
     
